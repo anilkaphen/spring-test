a2a795387815e6dd49d207a5f3566f8e29fea704
Anupamkaphen <Anupamkaphen@gmail.com>
2024-02-09 13:34:14 +0530

    added car controller

--- src/main/java/com/anupam/springtest/controller/CarController.java
+++ src/main/java/com/anupam/springtest/controller/CarController.java
@@ -1,13 +1,18 @@
 package com.anupam.springtest.controller;
+
 import com.anupam.springtest.modal.Car;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
+
 @RestController
 public class CarController {
     private List<Car> list = new ArrayList<>();
+
     @GetMapping("/car")
     public List<Car> getAllCarDetails() {
         list.add(new Car("a001", "Alto", 5555, "Red", "Maruti"));
@@ -17,38 +22,17 @@ public class CarController {
         list.add(new Car("a002", "indica", 10000, "Silver", "Tata"));
         return list;
     }
+
     @GetMapping("/car/brand/{brand}")
     public List<Car> getCarBrandDetails(@PathVariable String brand) {
         List<Car> carList = new ArrayList<>();
-        for (Car c :list) {
+        for (Car c : list) {
             if (c.getBrand().equals(brand)) ;
             carList.add(c);
         }
-        return  carList;
+        return carList;
     }
-    @PostMapping("/car")
-    public void updateCar(@RequestBody Car car) {
-        list.add(car);
-    }
-    @GetMapping("/car/{color}")
-    public List<Car> getCarDetailsBycolor(@PathVariable String color, List<Car> cars) {
-        List<Car> list1 = new ArrayList<>();
-        for (Car c : cars) {
-            if (c.getColor().equals(color)) ;
-            list1.add(c);
-        }
-        return list1;
 
-    }
-    @GetMapping("/car/{price}")
-    public List<Car> getCarDetailsByPrice(@PathVariable Integer price, List<Car> cars) {
-
-        List<Car> list1 = new ArrayList<>();
-        for (Car c : cars) {
-            if (c.getPrice().equals(price)) ;
-            list1.add(c);
-        }return list1;
-    }
     @PutMapping("/car")
     public ResponseEntity<Car> addPlayer(@RequestBody Car car) {
         try {
@@ -59,7 +43,21 @@ public class CarController {
             return new ResponseEntity(HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }
+
+    @GetMapping("/lowest/price")
+    public List<Car> getCarDetailsBycolor() {
+        return null;
+
+    }
+
+    @PostMapping("/car")
+    public void updateCar(@RequestBody Car car) {
+        list.add(car);
+    }
+
 }
+
+
 
 
 
