45438dcd1506759a762c79610908130105f7abd5
John Doe <anilkaphen@gmail.com>
2024-02-12 08:01:43 +0000

    Fixed issue

--- src/main/java/com/anupam/springtest/CarTest.java
+++ src/main/java/com/anupam/springtest/CarTest.java
@@ -19,14 +19,16 @@ public class CarTest {
         List<Car> colorCarList = carTest.getCarByColor("Blue");
         System.out.println("getCarByColor: \n" + colorCarList);
 
-        Long carAveragePrice = carTest.getCarAveragePrice("Alto");
+        Long carAveragePrice = carTest.getCarAveragePrice();
         System.out.println("getCarByprice: \n" + carAveragePrice);
-        List<Car> lowestprice = carTest.getCarlowestprice();
+
+        int lowestprice = carTest.getCarLowestPrice();
         System.out.println("getcarlowestprice: \n" + lowestprice);
 
-        List<Car> highestprice = carTest.getCarhighestprice1();
+        int highestprice = carTest.getCarHighestPrice();
         System.out.println("getcarhighesttprice: \n" + highestprice);
     }
+
     public List<Car> getAllCarDetails() {
         carList.add(new Car("a001", "Alto", 5555, "Red", "Maruti"));
         carList.add(new Car("a003", "zen", 6000, "Blue", "Maruti"));
@@ -45,6 +47,7 @@ public class CarTest {
         }
         return list;
     }
+
     public List<Car> getCarByColor(String color) {
         List<Car> list = new ArrayList<>();
         for (Car c : carList) {
@@ -54,6 +57,7 @@ public class CarTest {
         }
         return list;
     }
+
     public long getCarAveragePrice() {
         int sum = 0, avg = 0;
         for (int i = 0; i < carList.size(); i++) {
@@ -62,25 +66,26 @@ public class CarTest {
         avg = sum / carList.size();
         return avg;
     }
-    public List getCarlowestprice() {
-        int lowest = 99999, price = 0;
+
+    public int getCarLowestPrice() {
+        int lowest = 99999;
         for (Car c : carList) {
-            if (lowest < c.getPrice()) {
+            if (lowest > c.getPrice()) {
                 lowest = c.getPrice();
             }
-
         }
-        return  carList;
-
+        return lowest;
     }
-    public List getCarhighestprice1() {
 
-        int highest = 1000000, price = 0;
+    public int getCarHighestPrice() {
+        int highest = -1;
         for (Car c : carList) {
-            if (highest > c.getPrice()) {
+            if (highest < c.getPrice()) {
                 highest = c.getPrice();
             }
-            return  carList;
+        }
+        return highest;
+    }
 }
 
 
