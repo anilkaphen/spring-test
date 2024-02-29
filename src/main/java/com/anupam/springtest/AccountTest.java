a1ed08aa03f9be0b1a315502eface46293d40b73
Anupamkaphen <Anupamkaphen@gmail.com>
2024-02-29 12:27:05 +0530

    account details added

--- src/main/java/com/anupam/springtest/AccountTest.java
+++ src/main/java/com/anupam/springtest/AccountTest.java
@@ -23,21 +23,31 @@ public class AccountTest {
         List<SavingAccount> AccountByCity = accountTest.getAccountByCity("Patna");
         System.out.println("getAccountByCity: \n" + AccountByCity);
 
+        List<SavingAccount> EmployeByDobAndState = accountTest.getEmployeByDobAndState();
+        System.out.println("getEmployeByDobAndState =: \n" + EmployeByDobAndState);
+
+        List<SavingAccount> AccountByStateAndCity = accountTest.getEmployeeByStateAndCity("Bihar","Muz");
+        System.out.println("AccountByStateAndCity: \n" + AccountByStateAndCity);
+
         long AccountAverageBalance = accountTest.getAccountAverageBalance();
         System.out.println(" getAccountAverageBalance\n: " + AccountAverageBalance);
+        List<SavingAccount> WomenAccountOfBihar =       accountTest.getWomenAccountOfBihar();
 
-        System.out.println(" getAccountByGender\n: " + accountTest.getAccountByGender(false));
+        System.out.println(" getWomenAccountOfBihar\n: " + WomenAccountOfBihar);
+
+        //System.out.println(" getAccountByGender\n: " + accountTest.getAccountByGender(false));
     }
 
     public List<SavingAccount> saveAccounts() {
         List<SavingAccount> list = new ArrayList<>();
 
-        list.add(new SavingAccount("AC001", "Mohan", 10, "Patna", 10000, "Bihar", true, LocalDate.of(1992, 02, 15)));
+        list.add(new SavingAccount("AC001", "Mohan", 10, "Patna", 10000, "Bihar", true, LocalDate.of(1948, 02, 15)));
         list.add(new SavingAccount("AC002", "Sohan", 12, "Muz", 20000, "Bihar", true, LocalDate.of(1994, 03, 14)));
         list.add(new SavingAccount("AC003", "Ram", 14, "Muz", 5000, "Bihar", true, LocalDate.of(1996, 05, 12)));
         list.add(new SavingAccount("AC004", "Sita", 16, "Mumbai", 7000, "Maha", false, LocalDate.of(1998, 07, 10)));
         list.add(new SavingAccount("AC005", "Raja", 18, "Pune", 11000, "Bihar", true, LocalDate.of(1999, 10, 6)));
         list.add(new SavingAccount("AC006", "Gita", 21, "Delhi", 18000, "Delhi", false, LocalDate.of(2000, 04, 5)));
+        list.add(new SavingAccount("AC006", "Rita", 25, "Darbhangha", 19000, "Bihar", false, LocalDate.of(2001, 06, 9)));
 
         return list;
     }
@@ -89,7 +99,68 @@ public class AccountTest {
         }
         return list;
     }
-}
+    public List<SavingAccount> getAccountByState(String state) {
+        List<SavingAccount> list = new ArrayList<>();
+        for (SavingAccount s : accountDetails) {
+            if (s.getState().equals(state)) {
+                list.add(s);
+            }
+        }
+        return list;
+    }
+//getEmployeByDobAndState
+    public List<SavingAccount> getEmployeByDobAndState() {
+        List<SavingAccount> list = new ArrayList<>();
+        for (SavingAccount s : accountDetails) {
+            if (s.getState().equals("Bihar") && s.getDob().isAfter(LocalDate.of(1950, 04, 5))) {
+                list.add(s);
+            }
+        }
+        return list;
+    }
+
+    public List<SavingAccount> getEmployeeByStateAndCity( String state ,String city) {
+        List<SavingAccount> list = new ArrayList<>();
+        for (SavingAccount s : accountDetails) {
+            if (s.getState().equals(state) && s.getCity().equals(city));
+                list.add(s);
+            }
+        return list;
+    }
+
+
+    public  long getMinSalMaharashtra(List<SavingAccount> savingAccounts){
+        long lowest = 9999;
+        for(SavingAccount s :accountDetails){
+            if(lowest < s.getBalance()){
+                lowest= s.getBalance();
+            }
+
+        }
+        return  lowest;
+
+    }
+    public long getMaxSalBihar(List<SavingAccount> savingAccounts){
+        long maximum=-1;
+        for(SavingAccount s :accountDetails){
+            if(maximum>s.getBalance()){
+                maximum=s.getBalance();
+            }
+        }
+        return maximum;
+
+    }
+    public List<SavingAccount> getWomenAccountOfBihar( ) {
+        List<SavingAccount> list = new ArrayList<>();
+        for (SavingAccount s : accountDetails) {
+            if(s.isGender()== false && s.getState().equals("Bihar"))
+                list.add(s);
+
+        }
+
+
+        return list;
+    }}
 
 
 
