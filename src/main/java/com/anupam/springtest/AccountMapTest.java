7eb9017c4b5ea08907feaddf8bec83f69e538f9b
Anupamkaphen <Anupamkaphen@gmail.com>
2024-03-03 13:49:32 +0530

    Account map added

--- src/main/java/com/anupam/springtest/AccountMapTest.java
+++ src/main/java/com/anupam/springtest/AccountMapTest.java
@@ -9,21 +9,43 @@ import java.util.List;
 import java.util.Map;
 
 public class AccountMapTest {
+
+
     private static Map<String, SavingAccount> accountMap = new HashMap<>();
 
     public static void main(String[] args) {
+        AccountMapTest accountMapTest = new AccountMapTest();
+        Map<String, SavingAccount> saveAccounts = accountMapTest.saveAccounts();
+        System.out.println("saveAccounts: \n" + saveAccounts);
+        long savingAccountMap = accountMapTest.getMaxAccountBalance(accountMap);
+        System.out.println("getMaxAccountBalance: \n" + savingAccountMap);
+
 
     }
-    public List<SavingAccount> saveAccounts() {
-        List<SavingAccount> list = new ArrayList<>();
-
+    public Map<String, SavingAccount> saveAccounts() {
         accountMap.put("AC001", new SavingAccount("AC001", "Mohan", 10, "Patna", 10000, "Bihar", true, LocalDate.of(1992, 02, 15)));
         accountMap.put("AC002", new SavingAccount("AC002", "Sohan", 12, "Muz", 20000, "Bihar", true, LocalDate.of(1994, 03, 14)));
         accountMap.put("AC003", new SavingAccount("AC003", "Ram", 14, "Muz", 5000, "Bihar", true, LocalDate.of(1996, 05, 12)));
         accountMap.put("AC004", new SavingAccount("AC004", "Sita", 16, "Mumbai", 7000, "Maha", false, LocalDate.of(1998, 07, 10)));
         accountMap.put("AC005", new SavingAccount("AC005", "Raja", 18, "Pune", 11000, "Bihar", true, LocalDate.of(1999, 10, 6)));
         accountMap.put("AC006", new SavingAccount("AC006", "Gita", 21, "Delhi", 18000, "Delhi", false, LocalDate.of(2000, 04, 5)));
-
-        return list;
+        return accountMap;
     }
+    public long getMaxAccountBalance(Map<String, SavingAccount> map) {
+        long max = -1;
+        for (SavingAccount s : map.values()) {
+            if (max < s.getBalance()) {
+                max = s.getBalance();
+            }
+
+        }
+        return max;
+    }
+
+
 }
+
+
+
+
+
